e27be9c580f4ef5557afd0f2da27c8df86407a66
kent010341 <kent010341@gmail.com>
2019-12-06 15:38:05 +0800

    Update test.py

--- AnimatedPlot/test.py
+++ AnimatedPlot/test.py
@@ -10,4 +10,7 @@ y = x ** 2
 ap.plot(x, y, label='x^2')
 ap.scatter(x, x**3, label='x^3')
 ap.plot([-5, 5], [0, 0], fixed=True)
+ap.title('test')
+ap.xlabel('x label')
+ap.ylabel('y label')
 ap.show()
